aa2b20297234985f2c1f9c36ddee2f527494e8fd
Lucan de Groot <lucan.degroot@gmail.com>
2023-08-03 19:43:25 +0200

    Add dynamic macros

--- keyboards/gmmk/pro/rev1/ansi/keymaps/Lucan11/rules.mk
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/Lucan11/rules.mk
@@ -1,2 +1,3 @@
 DEBOUNCE_TYPE = sym_eager_pk			# No latency on keypress
 VIA_ENABLE = yes						# Enable VIA support, to change keybinds without flashing
+DYNAMIC_MACRO_ENABLE = yes				# Record macro's on-the-fly (lost on power cycle)
